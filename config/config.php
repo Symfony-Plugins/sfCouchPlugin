a0fc5f9da134fb9b47480b484da1ffa089c3db72
rick <rick@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2010-03-18 08:00:20 -0500

    git-svn-id: http://svn.symfony-project.com/plugins/sfCouchPlugin/trunk@28604 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- config/config.php
+++ config/config.php
@@ -8,9 +8,8 @@ $configCache = $this->getConfigCache();
 
 $couch_config_file = sfConfig::get('sf_config_dir').'/couchdb.yml';
 $configCache->registerConfigHandler($couch_config_file, 'sfDefineEnvironmentConfigHandler', array (
-  'prefix' => 'couchdb_',
+	'prefix' => 'couchdb_',
 ));
 if ($file = $configCache->checkConfig($couch_config_file, true)) {
-  include($file);
+	include($file);
 }
-
